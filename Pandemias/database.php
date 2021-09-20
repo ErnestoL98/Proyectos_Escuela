5e4562f100610f4c94bb47e64fd7bf69fa8dccbe
ErnestoLoAv <89870277+ErnestoL98@users.noreply.github.com>
2021-09-20 13:34:38 -0500

    Add files via upload

---
+++ Pandemias/database.php
@@ -0,0 +1,13 @@
+<?php
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'php_login_databse';
+
+
+    try{
+        $conn = new PDO("mysql:host=$server;dbname=$database;",$username,$password);
+    }catch(PDOException $e){
+        die('Conexion Fallida: '.$e->getMessage());
+    }
+?>
